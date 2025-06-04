f02639673b22dd29eb23829f725d78c80f7ac4ac
Kaloyan Borisov <kaborisov@tu-sofia.bg>
2025-06-04 21:59:44 +0300

    AAA

--- django/restaurant_backend/urls.py
+++ django/restaurant_backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+
 from django.views.static import serve
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -35,6 +35,6 @@ urlpatterns = [
 ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT}),
+    path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
