e93c85ef17b66114b7682d7aa0551b492f8fd097
Kaloyan Borisov <kaborisov@tu-sofia.bg>
2025-06-04 21:31:05 +0300

    off debug in prod

--- django/restaurant_backend/settings.py
+++ django/restaurant_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jcwan6%v9@etm$14z&l64m!7gokdc7t=)l-+j=#8(k0g3!85j!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
