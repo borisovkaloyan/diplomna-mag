b87c62069c71274593272f9d4be6154b48eb4292
Kaloyan Borisov <kaborisov@tu-sofia.bg>
2025-06-04 21:45:52 +0300

    fuck safety

--- django/restaurant_backend/settings.py
+++ django/restaurant_backend/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+
+# fuck safety
+
+from django.views.static import serve
+serve()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
